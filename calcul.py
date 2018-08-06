fa4831ed0e553cfb39983e7ba362c27ca3c2c800
Ludovic SOUPLET <ludovic.s@protonmail.com>
2018-08-06 17:04:36 +0200

    modification affichage du fichier calcul.py et ajout de la multiplication

--- calcul.py
+++ calcul.py
@@ -1,1 +1,4 @@
+print("Effectuez une addition")
 print(int(input("Entrez le nombre 1: ")) + int(input("Entrez le nombre 2:")))
+print("Effectuez une multiplication")
+print(int(input("Entrez le nombre 1: ")) * int(input("Entrez le nombre 2:")))
